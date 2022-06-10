a3499b2c3d3cf4ae5826b98f6827e3cdd45d1791
emlprime <emlprime@gmail.com>
2022-06-10 15:31:32 -0700

    Removed Google Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
 import './style.css';
 import Fight from './Fight';
-import { getCurrentUser } from './CurrentUser'
+import { getCurrentUser } from './CurrentUser';
 
 // Configure Firebase.
 //TODO remove api key from github
 const config = {
   apiKey: 'AIzaSyBtTdVWE1AZ1JRENUUiAlzqlIXsaTVhCkc',
   authDomain: 'nidaviller-fe.firebaseapp.com',
-  // ...
+  projectId: 'nidaviller-fe',
 };
-firebase.initializeApp(config);
-
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-  callbacks: {
-    // Avoid redirects after sign-in.
-    signInSuccessWithAuthResult: () => false,
-  },
-};
-
 
 function App() {
-  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+  const [isSignedIn, setIsSignedIn] = useState(true); // Local signed-in state.
 
-  // Listen to the Firebase Auth state and set the local state.
-  useEffect(() => {
-    const unregisterAuthObserver = firebase
-      .auth()
-      .onAuthStateChanged((user) => {
-        setIsSignedIn(!!user);
-      });
-    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
-  }, []);
-  const currentUser = getCurrentUser(firebase)
   if (!isSignedIn) {
     return (
       <div>
@@ -57,12 +30,7 @@ function App() {
   return (
     <div>
       <h1>Epicurean Brawl</h1>
-      <p>
-        Welcome {currentUser.displayName}! Get ready to RUMBLE!
-      </p>
-      <button type="button" onClick={() => firebase.auth().signOut()}>
-        Sign-out
-      </button>
+
       <Style>
         <Fight />
       </Style>
